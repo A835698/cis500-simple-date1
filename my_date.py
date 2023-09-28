40366e88dcaf2aa5a24ec2f10969549abc62c1c9
A835698 <144069661+A835698@users.noreply.github.com>
2023-09-28 18:40:20 -0400

    Grade Me

--- my_date.py
+++ my_date.py
@@ -1,8 +1,8 @@
 #######################################################
 # my_date
 #
-# Name: zzNAMEzz (replace with your name)
-# Section: XX
+# Name: (Avinash Ponnada)
+# Section: 03
 #
 # Fall 2023
 #########################################################
@@ -10,19 +10,41 @@
 
 def is_leap_year(year: int) -> bool:
     """Return True if year is a leap year, False otherwise"""
-    pass
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        return True
+    return False
+
  
 def ordinal_date(year:int , month: int, day: int) -> int:
     """ Return the number of days elapsed since the beginning of the year, including any partial days.
         For example, the ordinal date for 1 January is 1.
         Hint: pre-compute the ordinal date for the first of each month."""
-    pass
+    day_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    #in leap year (February has 29 days)
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        day_in_month[2] = 29
+    ordinal = day
+    for i in range(1, month):
+        ordinal += day_in_month[i]
+
+    return ordinal
+
 
 def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, day2:int ) -> int:
     """ Return the number of days that have elapsed between year1-month1-day1 and year2-month2-day2.
         You may assume that year1-month1-day1 falls on or before year2-month2-day2. (In other words,
         your answer will always be >= 0.) """
-    pass
+    import datetime
+    date1 = datetime.date(year1, month1, day1)
+    date2 = datetime.date(year2, month2, day2)
+    diff = date2 - date1
+    return diff.days
 
 # This is a tuple. It is immutable so that users can't accidentally modify it.
 DAYS_OF_WEEK = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
@@ -31,10 +53,18 @@ def day_of_week(year: int, month: int, day: int) -> str:
     """ Return the day of the week (Sunday, Monday, Tuesday, etc.) for the given day
         Hint 1: 1 January 1753 was a Monday.
         Hint 2: Use the methods you've already written."""
-    pass
+    import datetime
+    date_object = datetime.date(year, month, day)
+    index = date_object.weekday()
+    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return days_of_week[index]
     
 def to_str(year: int, month: int, day: int) -> str:
     """ Return this date as string of the form "Wednesday, 07 March 1833"."""
-    pass
+    import datetime
+    date_object = datetime.date(year, month, day)
+    str_date = date_object.strftime("%A, %d %B %Y")
+    return str_date
+
               
     
--- my_date_test.py
+++ my_date_test.py
@@ -3,51 +3,92 @@ import my_date
 
 class MyDateTest(unittest.TestCase):
 
-    #
-    # is_leap_year
-    #
+    def test_divided_by_4(self):
+        self.assertTrue(my_date.is_leap_year(2040))
+        self.assertTrue(my_date.is_leap_year(2016))
+        self.assertTrue(my_date.is_leap_year(3200))
 
-    def test_is_leap_year1(self):
-        self.assertTrue(my_date.is_leap_year(1984))
+    def test_divided_by_100_and_400(self):
+        self.assertTrue(my_date.is_leap_year(1600))
+        self.assertTrue(my_date.is_leap_year(2800))
 
-    def test_is_leap_year2(self):
-        self.assertFalse(my_date.is_leap_year(1985))
+    def test_divided_by_100_but_not_400(self):
+        self.assertFalse(my_date.is_leap_year(1700))
+        self.assertFalse(my_date.is_leap_year(2300))
 
-    #
-    # ordinal_date
-    #
+    def test_not_divided_by_4(self):
+        self.assertFalse(my_date.is_leap_year(2027))
+        self.assertFalse(my_date.is_leap_year(1997))
 
-    def test_ordinal_date1(self):
-        self.assertEqual(1, my_date.ordinal_date(1997, 1, 1))
+   
+    def test_jan_1(self):
+        self.assertEqual(my_date.ordinal_date(2023, 1, 1), 1)
 
-    def test_ordinal_date2(self):
-        self.assertEqual(32, my_date.ordinal_date(1995, 2, 1))
+    def test_feb_15(self):
+        self.assertEqual(my_date.ordinal_date(2023, 2, 15), 46)
 
-    #
-    # days_elapsed  
-    #
+    def test_feb_29(self):
+        self.assertEqual(my_date.ordinal_date(2024, 2, 29), 60)
 
-    def test_days_elapsed1(self):
-        self.assertEqual(1, my_date.days_elapsed(1997, 1, 1, 1997, 2, 1))
+    def test_dec_31(self):
+        self.assertEqual(my_date.ordinal_date(2023, 12, 31), 365)
 
-    def test_days_elapsed2(self):
-        self.assertEqual(31, my_date.days_elapsed(1997, 1, 1, 1997, 2, 1))
+    def test_same_dates(self):
+        self.assertEqual(my_date.days_elapsed(2023, 7, 24, 2023, 7, 24), 0)
 
-    def test_days_elapsed3(self):
-        self.assertEqual(365, my_date.days_elapsed(1997, 1, 1, 1998, 1, 1))
+    def test_consecutive_dates(self):
+        self.assertEqual(my_date.days_elapsed(2023, 7, 23, 2023, 7, 24), 1)
 
-    # Warning: You need more tests than you think for this function
+    def test_same_year_dates(self):
+        self.assertEqual(my_date.days_elapsed(2023, 7, 23, 2023, 12, 31), 161)
 
-    #
-    # day_of_week
-    #
+    def test_different_years(self):
+        self.assertEqual(my_date.days_elapsed(2023, 9, 22, 2024, 9, 22), 366)
 
-    def test_day_of_week1(self):
-        self.assertEqual("Monday", my_date.day_of_week(1753, 1, 1))
+    def test_leapyear_dates(self):
+        self.assertEqual(my_date.days_elapsed(2024, 2, 29, 2024, 12, 31), 306)
+    
+    def test_monday(self):
+        self.assertEqual(my_date.day_of_week(2023, 9, 25), "Monday")
+
+    def test_sunday(self):
+        self.assertEqual(my_date.day_of_week(2023, 9, 18), "Monday")
+
+    def test_saturday(self):
+        self.assertEqual(my_date.day_of_week(2023, 9, 23), "Saturday")
+
+    def test_wednesday(self):
+        self.assertEqual(my_date.day_of_week(2022, 12, 7), "Wednesday")
+
+    def test_thursday(self):
+        self.assertEqual(my_date.day_of_week(2024, 2, 29), "Thursday")
+
+    def test_friday(self):
+        self.assertEqual(my_date.day_of_week(2025, 8, 15), "Friday")
+
+    def test_tuesday(self):
+        self.assertEqual(my_date.day_of_week(2000, 5, 2), "Tuesday")
+
+    def test_monday(self):
+        self.assertEqual(my_date.to_str(2023, 9, 18), "Monday, 18 September 2023")
+
+    def test_tuesday(self):
+        self.assertEqual(my_date.to_str(2023, 9, 19), "Tuesday, 19 September 2023")
+
+    def test_wednesday(self):
+        self.assertEqual(my_date.to_str(2023, 9, 20), "Wednesday, 20 September 2023")
+
+    def test_thursday(self):
+        self.assertEqual(my_date.to_str(2023, 9, 21), "Thursday, 21 September 2023")
+
+    def test_friday(self):
+        self.assertEqual(my_date.to_str(2023, 9, 22), "Friday, 22 September 2023")
+
+    def test_saturday(self):
+        self.assertEqual(my_date.to_str(2023, 9, 23), "Saturday, 23 September 2023")
 
 
-    #
-    # to_str
-    #
-    def test_to_str1(self):
-        self.assertEqual("Wednesday, 02 August 2024", my_date.to_str(2024, 8, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
